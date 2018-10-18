a43b14aa8de1d79f3428b73f895b9f246e81d16b
PrajwalM2212 <prajwalmmath@gmail.com>
2018-10-18 20:55:29 +0530

    plugin

--- docs/level-11-jinja2-cli/moban-string-temp/moban-string-temp/__init__.py
+++ docs/level-11-jinja2-cli/moban-string-temp/moban-string-temp/__init__.py
@@ -1,19 +1,18 @@
-from lml.plugin import PluginInfo
-import moban.constants as constants
 import os
+from collections import defaultdict
+from lml.plugin import PluginInfo
+from moban.hashstore import HASH_STORE
+import moban.utils as utils
+import moban.constants as constants
 import moban.exceptions as exceptions
 import moban.reporter as reporter
 from string import Template
-import moban.utils as utils
-from moban.hashstore import HASH_STORE
-from collections import defaultdict
 
 
 @PluginInfo(
     constants.TEMPLATE_ENGINE_EXTENSION, tags=["stmp"]
 )
 class StringTemplateEngine(object):
-
     def __init__(self, template_dirs, context_dirs):
         verify_the_existence_of_directories(template_dirs)
         self.context = Context(context_dirs)
@@ -22,13 +21,11 @@ class StringTemplateEngine(object):
         self.__templated_count = 0
 
     def render_to_file(self, template_file, data_file, output_file):
-        handler = open(template_file)
-        template = handler.read()
+        template = self.jj2_environment.get_template(template_file)
         data = self.context.get_data(data_file)
         reporter.report_templating(template_file, output_file)
 
-        s = Template(template)
-        rendered_content = s.substitute(**data)
+        rendered_content = template.render(**data)
         utils.write_file_out(output_file, rendered_content)
         self._file_permissions_copy(template_file, output_file)
 
@@ -36,6 +33,7 @@ class StringTemplateEngine(object):
         sta = Strategy(array_of_param_tuple)
         sta.process()
         choice = sta.what_to_do()
+        # based on choice decide what to do
         if choice == Strategy.DATA_FIRST:
             self._render_with_finding_data_first(sta.data_file_index)
         else:
@@ -56,11 +54,9 @@ class StringTemplateEngine(object):
 
     def _render_with_finding_template_first(self, template_file_index):
         for (template_file, data_output_pairs) in template_file_index.items():
-            handler = open(template_file)
-            template = handler.read()
             for (data_file, output) in data_output_pairs:
                 data = self.context.get_data(data_file)
-                flag = self._apply_template(template, data, output)
+                flag = self._apply_template(template_file, data, output)
                 if flag:
                     reporter.report_templating(template_file, output)
                     self.__templated_count += 1
@@ -70,27 +66,24 @@ class StringTemplateEngine(object):
         for (data_file, template_output_pairs) in data_file_index.items():
             data = self.context.get_data(data_file)
             for (template_file, output) in template_output_pairs:
-                handler = open(template_file)
-                template = handler.read()
-                flag = self._apply_template(template, data, output)
+                flag = self._apply_template(template_file, data, output)
                 if flag:
                     reporter.report_templating(template_file, output)
                     self.__templated_count += 1
                 self.__file_count += 1
 
-    def _apply_template(self, template, data, output):
-        s = Template(template)
-        rendered_content = s.substitute(**data)
+    def _apply_template(self, template_file, data, output):
+        rendered_content = Template(open(template_file).read()).substitute(**data)
         rendered_content = utils.strip_off_trailing_new_lines(rendered_content)
         rendered_content = rendered_content.encode("utf-8")
         flag = HASH_STORE.is_file_changed(
-            output, rendered_content, template.filename
+            output, rendered_content, template_file.filename
         )
         if flag:
             utils.write_file_out(
                 output, rendered_content, strip=False, encode=False
             )
-            utils.file_permissions_copy(template.filename, output)
+            utils.file_permissions_copy(template_file.filename, output)
         return flag
 
     def _file_permissions_copy(self, template_file, output_file):
@@ -101,7 +94,7 @@ class StringTemplateEngine(object):
                 break
         utils.file_permissions_copy(true_template_file, output_file)
 
-
+# specifically for data/config files
 class Context(object):
     def __init__(self, context_dirs):
         verify_the_existence_of_directories(context_dirs)
@@ -120,11 +113,13 @@ class Strategy(object):
     DATA_FIRST = 1
     TEMPLATE_FIRST = 2
 
+    # data_file_index and template_file_index are dict of lists
     def __init__(self, array_of_param_tuple):
         self.data_file_index = defaultdict(list)
         self.template_file_index = defaultdict(list)
         self.tuples = array_of_param_tuple
 
+    # All this is doing is dict[datafile] = (temp_file,out_file) and dict[temp_file] = (data_file,out_file)
     def process(self):
         for (template_file, data_file, output_file) in self.tuples:
             _append_to_array_item_to_dictionary_key(
@@ -135,19 +130,21 @@ class Strategy(object):
                 template_file,
                 (data_file, output_file),
             )
-
+    # Decide what should be first
     def what_to_do(self):
         choice = Strategy.DATA_FIRST
+        # if data_file_index is empty then template first
         if self.data_file_index == {}:
             choice = Strategy.TEMPLATE_FIRST
         elif self.template_file_index != {}:
+            # if template_file_index is not empty and data_files > template_files then template_first
             data_files = len(self.data_file_index)
             template_files = len(self.template_file_index)
             if data_files > template_files:
                 choice = Strategy.TEMPLATE_FIRST
         return choice
 
-
+# All this is doing is dict[datafile] = (temp_file,out_file) and dict[temp_file] = (data_file,out_file)
 def _append_to_array_item_to_dictionary_key(adict, key, array_item):
     if array_item in adict[key]:
         raise exceptions.MobanfileGrammarException(
@@ -156,7 +153,7 @@ def _append_to_array_item_to_dictionary_key(adict, key, array_item):
     else:
         adict[key].append(array_item)
 
-
+# decide if directory exists
 def verify_the_existence_of_directories(dirs):
     if not isinstance(dirs, list):
         dirs = [dirs]
@@ -164,8 +161,8 @@ def verify_the_existence_of_directories(dirs):
         if os.path.exists(directory):
             continue
         should_I_ignore = (
-                constants.DEFAULT_CONFIGURATION_DIRNAME in directory
-                or constants.DEFAULT_TEMPLATE_DIRNAME in directory
+            constants.DEFAULT_CONFIGURATION_DIRNAME in directory
+            or constants.DEFAULT_TEMPLATE_DIRNAME in directory
         )
         if should_I_ignore:
             # ignore
